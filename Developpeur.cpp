3c2d1f792636b918216505637975e0079ae1e267
Edouard-all <eefj.allaire@gmail.com>
2026-02-11 16:02:07 -0500

    EA: partie developpeur terminee non testee

---
+++ Developpeur.cpp
@@ -0,0 +1,69 @@
+#include "Developpeur.hpp"
+#include "Jeu.hpp"
+#include <span>
+#include <iostream>
+#include "cppitertools/enumerate.hpp"
+
+using namespace std;
+using namespace iter;
+
+Developpeur::Developpeur() {
+	
+}
+
+Developpeur::Developpeur(const std::string& nom) {
+	paireNomJeux_.first = nom;
+	paireNomJeux_.second.capacite = 0;
+	paireNomJeux_.second.elements = new Jeu*;
+	paireNomJeux_.second.nElements = 0;
+}
+
+Developpeur::~Developpeur() {
+	delete[] paireNomJeux_.second.elements;
+}
+
+const std::string& Developpeur::obtenirNom() const {
+	return paireNomJeux_.first;
+}
+
+uint8_t Developpeur::compterNombreJeuxDeveloppe(const ListeJeux& listeJeux) const {
+	span<Jeu*> listeJeuSpanne(listeJeux.elements, listeJeux.nElements);
+	uint8_t compteur = 0;
+	for (Jeu* jeu : listeJeuSpanne) {
+		if (jeu->developpeur == paireNomJeux_.first) {
+			compteur++;
+		}
+	}
+	return compteur;
+}
+
+void Developpeur::mettreAJourListeJeuxDeveloppeur(const ListeJeux& listeJeux) {
+	span<Jeu*> listeJeuSpanne(listeJeux.elements, listeJeux.nElements);
+	span<Jeu*> listeJeuSpanneDeveloppeur(paireNomJeux_.second.elements, paireNomJeux_.second.nElements);
+	ListeJeux listeJeuxDeveloppeur;
+	uint8_t nJeuxDeveloppeur = compterNombreJeuxDeveloppe(listeJeux);
+	listeJeuxDeveloppeur.elements = new Jeu* [nJeuxDeveloppeur];
+	listeJeuxDeveloppeur.capacite = nJeuxDeveloppeur;
+	listeJeuxDeveloppeur.nElements = 0;
+	for (auto [i, jeu] : enumerate(listeJeuSpanneDeveloppeur)) {
+		listeJeuxDeveloppeur.elements[i] = jeu;
+		listeJeuxDeveloppeur.nElements++;
+	}
+	for (Jeu* jeu : listeJeuSpanne) {
+		for (Jeu* jeuDeveloppeur : listeJeuSpanneDeveloppeur) {
+			uint8_t compteur = 0;
+			if (jeu->titre != jeuDeveloppeur->titre) {
+				listeJeuxDeveloppeur.elements[paireNomJeux_.second.nElements + compteur] = jeu;
+				compteur++;
+			}
+		}
+	}
+	paireNomJeux_.second = listeJeuxDeveloppeur;
+}
+
+void Developpeur::afficherJeuxDeveloppe() const {
+	span<Jeu*> listeJeuSpanne(paireNomJeux_.second.elements, paireNomJeux_.second.nElements);
+	for (Jeu* jeu : listeJeuSpanne) {
+		cout << jeu->titre;
+	}
+}
