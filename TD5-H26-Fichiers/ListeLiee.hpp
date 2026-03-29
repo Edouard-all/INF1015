1138c01e9275f7c10b4983bb6c8d623d2ad5f865
Thierno Aliou Baniré Diallo <thierno-aliou-banire.diallo@etud.polymtl.ca>
2026-03-29 12:05:46 -0400

    Implementation de push_back et de insert dans liste lie

--- TD5-H26-Fichiers/ListeLiee.hpp
+++ TD5-H26-Fichiers/ListeLiee.hpp
@@ -11,9 +11,9 @@ public:
 
 	//TODO: La construction par défaut doit créer une liste vide valide.
 	ListeLiee() {
-		/*tete_ = nullptr;
+		tete_ = nullptr;
 		queue_ = nullptr;
-		taille_ = 0;*/
+		taille_ = 0;
 	}
 	~ListeLiee()
 	{
@@ -21,10 +21,10 @@ public:
 		// Pour enlever la tête, 
 		// 1. La tête doit devenir le suivant de la tête actuelle.
 		// 2. Ne pas oublier de désallouer le noeud de l'ancienne tête (si pas fait automatiquement).
-		/*Noeud<T>* teteTemporaire;
+		Noeud<T>* teteTemporaire;
 		teteTemporaire = tete_->apres_;
 		delete tete_;
-		tete_ = teteTemporaire;*/
+		tete_ = teteTemporaire;
 	}
 
 	bool estVide() const  { return taille_ == 0; }
@@ -39,6 +39,18 @@ public:
 		//TODO: Vous devez créer un nouveau noeud en mémoire.
 		//TODO: Si la liste était vide, ce nouveau noeud est la tête et la queue;
 		// autrement, ajustez la queue et pointeur(s) adjacent(s) en conséquence.
+		Noeud<T>* nouveauNoeud; new Noeud(item, queue_, nullptr);
+		if (taille_) {
+			nouveauNoeud->donne_ = item;
+			nouveauNoeud->avant_ = queue_;
+			queue_ = nouveauNoeud;
+		}
+		else {
+			nouveauNoeud->donne_ = item;
+			tete_ = nouveauNoeud;
+			queue_ = nouveauNoeud;
+		}
+
 	}
 
 	// Insère avant la position de l'itérateur.
@@ -57,6 +69,20 @@ public:
 		//    (précédent de l'itérateur) afin qu'il point vers le noeud créé.
 		// 5. Incrémentez la taille de la liste.
 		// 6. Retournez un nouvel itérateur initialisé au nouveau noeud.
+		Noeud<T> nouveauNoeud;
+		nouveauNoeud.donne_ = item;
+		nouveauNoeud.apres_ = it.position_;
+		it.reculer();
+		nouveauNoeud.avant_ = it.position_;
+
+		nouveauNoeud.avant_->apres_ = &nouveauNoeud;
+		nouveauNoeud.apres_->avant_ = &nouveauNoeud;
+
+		taille_++;
+		return iterator(&nouveauNoeud);
+
+
+
 	}
 
 	// Enlève l'élément à la position it et retourne un itérateur vers le suivant.
@@ -76,6 +102,7 @@ public:
 		//  donc en 2. il se peut qu'il n'y ait pas de précédent et alors c'est
 		//  la tête de liste qu'il faut ajuster.
 		//NOTE: On ne demande pas de supporter d'effacer le dernier élément (c'est similaire au cas pour enlever le premier).
+
 	}
 
 private:
